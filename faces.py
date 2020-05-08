64d2879731e52440b40343feb2bdb9858fe23b12
Jonastuhh <eckhoff-jonas@web.de>
2020-05-08 16:05:26 +0200

    Create faces.py

---
+++ faces.py
@@ -0,0 +1,18 @@
+import face_recognition
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image  
+
+img = cv2.imread('faces.jpg')
+
+face_locations = face_recognition.face_locations(img)
+
+
+for face_coords in face_locations:
+
+    
+    face=img[face_coords[0]:face_coords[2],face_coords[3]:face_coords[1]]
+    face_48=cv2.resize(face,(48,48),interpolation = cv2.INTER_AREA)
+    face_48_gr = cv2.cvtColor(face_48, cv2.COLOR_BGR2GRAY)
+    Image.fromarray(face_48_gr).show()
