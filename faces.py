0effce3f9687efe4db6c8f2f3bf4400566f663b1
Max Lewerenz <max.lewerenz@gmail.com>
2020-05-08 16:48:22 +0200

    Test

--- faces.py
+++ faces.py
@@ -10,9 +10,9 @@ face_locations = face_recognition.face_locations(img)
 
 
 for face_coords in face_locations:
-
-    
     face=img[face_coords[0]:face_coords[2],face_coords[3]:face_coords[1]]
     face_48=cv2.resize(face,(48,48),interpolation = cv2.INTER_AREA)
     face_48_gr = cv2.cvtColor(face_48, cv2.COLOR_BGR2GRAY)
     Image.fromarray(face_48_gr).show()
+
+#Hallo this is a Comment
